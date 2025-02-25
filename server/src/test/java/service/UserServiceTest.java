95343226c25d5ecf4c261c44c41d84db5de8b481
Elaina Hales <evhales@byu.edu>
2025-02-25 12:18:24 -0700

    creating userService register and login tests

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,28 +1,67 @@
 package service;
 
-import org.junit.jupiter.api.AfterEach;
+import dataaccess.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import requestsresults.RegisterRequest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import requestsresults.*;
 
 class UserServiceTest {
     private UserService service;
+    private UserDAO user;
+    private AuthDAO auth;
 
     @BeforeEach
     void setUp() {
         service = new UserService();
+        user = new MemoryUser();
+        auth = new MemoryAuth();
     }
 
     @Test
-    void register() {
+    void testRegisterSuccess() {
         RegisterRequest req = new RegisterRequest("test", "password", "test@gmail.com");
-
+        Assertions.assertDoesNotThrow(() -> {
+            service.register(req, user, auth);
+        });
     }
 
     @Test
-    void login() {
+    void testRegisterAlreadyTaken() throws BadReqException, AlreadyTakenException {
+        RegisterRequest req = new RegisterRequest("test", "password", "test@gmail.com");
+        service.register(req, user, auth);
+
+        AlreadyTakenException exception = Assertions.assertThrows(AlreadyTakenException.class, () -> {
+            service.register(req, user, auth);
+        });
+
+        Assertions.assertEquals("Error: already taken", exception.getMessage());
+    }
+
+    @Test
+    void testLoginSuccess() throws BadReqException, AlreadyTakenException {
+        RegisterRequest req = new RegisterRequest("test", "password", "test@gmail.com");
+        service.register(req, user, auth);
+
+        LoginRequest loginReq = new LoginRequest("test", "password");
+
+        Assertions.assertDoesNotThrow(() -> {
+            service.login(loginReq, user, auth);
+        });
+    }
+
+    @Test
+    void testLoginUnauthorized() throws BadReqException, AlreadyTakenException {
+        RegisterRequest req = new RegisterRequest("test", "password", "test@gmail.com");
+        service.register(req, user, auth);
+
+        LoginRequest loginReq = new LoginRequest("test", "idk");
+
+        UnauthorizedException exception = Assertions.assertThrows(UnauthorizedException.class, () -> {
+            service.login(loginReq, user, auth);
+        });
+
+        Assertions.assertEquals("Error: unauthorized", exception.getMessage());
     }
 
     @Test
