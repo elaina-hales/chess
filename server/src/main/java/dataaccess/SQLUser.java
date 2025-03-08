f66f1bf6be12792e6c5e548302c1beff416561e2
Elaina Hales <evhales@byu.edu>
2025-03-08 13:13:26 -0700

    writing createUser and getUser

--- server/src/main/java/dataaccess/SQLUser.java
+++ server/src/main/java/dataaccess/SQLUser.java
@@ -1,22 +1,51 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.HashMap;
 
 
 public class SQLUser implements UserDAO{
-    final private HashMap<String, UserData> users = new HashMap<>();
 
     @Override
     public void createUser(UserData user) {
-        user = new UserData(user.username(), user.password(), user.email());
-        users.put(user.username(), user);
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        String username = user.username();
+        String email = user.email();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO User (Username, Password, Email) VALUES (?, ?, ?)";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, hashedPassword);
+                preparedStatement.setString(3, email);
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public UserData getUser(String username) {
-        return users.get(username);
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT Username, password, email FROM User WHERE Username=?";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1, username);
+                try (var rs = preparedStatement.executeQuery()) {
+                    while (rs.next()) {
+                        String u = rs.getString("Username");
+                        String p = rs.getString("Password");
+                        String e = rs.getString("Email");
+
+                        return new UserData(u, p, e);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
